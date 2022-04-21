6f7a95823223085b2260839c506835e01f9f85f5
Ján Kľuka <jan.kluka@fmph.uniba.sk>
2022-04-21 12:44:07 +0200

    Updates

---
+++ precache-manifest.33efb1b0d30f5514d3976c83b496ce78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13997f084edc33597a04224d75db4ab",
+    "url": "/formalization-checker/index.html"
+  },
+  {
+    "revision": "2b965484d110fd01e622",
+    "url": "/formalization-checker/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "764b39fef7243c1bc6e8",
+    "url": "/formalization-checker/static/css/main.b7de0216.chunk.css"
+  },
+  {
+    "revision": "2b965484d110fd01e622",
+    "url": "/formalization-checker/static/js/2.cf21fb13.chunk.js"
+  },
+  {
+    "revision": "b9aa32ad4d98fbb6880a92ba641f2f93",
+    "url": "/formalization-checker/static/js/2.cf21fb13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "764b39fef7243c1bc6e8",
+    "url": "/formalization-checker/static/js/main.5d857312.chunk.js"
+  },
+  {
+    "revision": "a4d4a3bfe42b321e7221",
+    "url": "/formalization-checker/static/js/runtime-main.4ff20a1a.js"
+  }
+]);
