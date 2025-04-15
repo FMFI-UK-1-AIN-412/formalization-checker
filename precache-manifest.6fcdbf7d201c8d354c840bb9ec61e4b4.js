9e17732833141e6a8ddaabaf7bd13bab0e9f6883
Ján Kľuka <jan.kluka@fmph.uniba.sk>
2025-04-15 23:11:50 +0200

    Deploying 9451fa0 Predeploy script imporoved

---
+++ precache-manifest.6fcdbf7d201c8d354c840bb9ec61e4b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3e82b8baca591148d43ad3ed8fe532",
+    "url": "/formalization-checker/index.html"
+  },
+  {
+    "revision": "2051db2de748d5bf2738",
+    "url": "/formalization-checker/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "eee1f7a3321aa29da2fa",
+    "url": "/formalization-checker/static/css/main.636b17c6.chunk.css"
+  },
+  {
+    "revision": "2051db2de748d5bf2738",
+    "url": "/formalization-checker/static/js/2.d5e9a43b.chunk.js"
+  },
+  {
+    "revision": "b9aa32ad4d98fbb6880a92ba641f2f93",
+    "url": "/formalization-checker/static/js/2.d5e9a43b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee1f7a3321aa29da2fa",
+    "url": "/formalization-checker/static/js/main.68526a83.chunk.js"
+  },
+  {
+    "revision": "b340e52a9e81e1d851bd",
+    "url": "/formalization-checker/static/js/runtime-main.8401f60a.js"
+  }
+]);
